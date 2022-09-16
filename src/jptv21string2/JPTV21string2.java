32cf18d42ef8a76f0585f6a0571c205787795e56
pupil <pupil@ivkhk.ee>
2022-09-16 11:20:05 +0300

    first commit

---
+++ src/jptv21string2/JPTV21string2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv21string2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class JPTV21string2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("-----bukvi alfavita-----");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedite stroku:");
+        String str = scanner.nextLine();
+        String Alphabet = ("abcdefghijklomnpkqstyvuwyz");
+        
+    }
+    
+}
